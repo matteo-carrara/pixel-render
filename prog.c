587c5cf6b0212fb8270e4d4780a34b35977808ab
matteo carrara <matteo.carrara.7@gmail.com>
2023-09-17 22:45:07 +0200

    Updated code & created gitignore

--- prog.c
+++ prog.c
@@ -1,5 +1,9 @@
 #include <SDL2/SDL.h>
 
+
+
+
+
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL initialization failed: %s\n", SDL_GetError());
@@ -21,6 +25,12 @@ int main(int argc, char* argv[]) {
     SDL_Event event;
     int quit = 0;
 
+	// Set the background color to black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // R=0, G=0, B=0, A=255
+
+	// Clear the screen with the background color (black)
+	SDL_RenderClear(renderer);
+
     while (!quit) {
         while (SDL_PollEvent(&event) != 0) {
             if (event.type == SDL_QUIT) {
@@ -28,22 +38,13 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Set the background color to black
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // R=0, G=0, B=0, A=255
 
-        // Clear the screen with the background color (black)
-        SDL_RenderClear(renderer);
 
-        // Draw your pixels or shapes here
-        // For example, drawing a red pixel at (100, 100)
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set the drawing color to red
-    // Draw a red pixel at (100, 100)
-    for(int i = 0; i < 100; i++) {
-    	SDL_RenderDrawPoint(renderer, 100+i, 100);
-
-    	// Update the screen
-    	SDL_RenderPresent(renderer);
-    }
+		for(int i = 0; i < 100; i++) {
+			SDL_RenderDrawPoint(renderer, 100+i, 100);
+			SDL_RenderPresent(renderer);
+		}
     }
 
     SDL_DestroyRenderer(renderer);
