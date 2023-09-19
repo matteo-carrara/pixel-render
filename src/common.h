8648cc0e247f0644da3a97b70231ec35ef9c475e
matteo carrara <matteo.carrara.7@gmail.com>
2023-09-19 12:30:59 +0200

    Deleted c implementation

--- src/common.h
+++
@@ -1,5 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-extern int res_x;
-extern int res_y;
-#endif
--- src/draw.h
+++
@@ -1,1 +0,0 @@
-void drawLine(SDL_Renderer* renderer, int startx, int starty, int lenght, float slope);
