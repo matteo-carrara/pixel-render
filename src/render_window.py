a825624524ebfc16d440deda19fd1717e1524eb4
matteo carrara <matteo.carrara.7@gmail.com>
2023-09-19 15:15:36 +0200

    Added sample settings window

--- src/render_window.py
+++ src/render_window.py
@@ -1,9 +1,13 @@
+from re import S
 import pygame
+import tkinter as tk
+from tkinter import ttk
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 import time
+import threading
 
 from shader import *
 
@@ -13,6 +17,33 @@ class RenderWindow:
         
     def __init__(self):
         pass
+    
+    def _settingsWindow(self):
+        self.root = tk.Tk()
+        self.root.title("Parameter Controls")
+        self.root.geometry("400x300")
+        
+        # Create a Label for the first slider
+        label1 = tk.Label(self.root, text="Parameter 1")
+        label1.grid(row=0, column=0, padx=10, pady=10)
+
+        # Create the first slider
+        slider1 = ttk.Scale(self.root, from_=0, to=100, orient="horizontal")
+        slider1.grid(row=0, column=1, padx=10, pady=10)
+
+        # Create a Label for the second slider
+        label2 = tk.Label(self.root, text="Parameter 2")
+        label2.grid(row=1, column=0, padx=10, pady=10)
+
+        # Create the second slider
+        slider2 = ttk.Scale(self.root, from_=0, to=100, orient="horizontal")
+        slider2.grid(row=1, column=1, padx=10, pady=10)
+
+        # Create a button
+        button = ttk.Button(self.root, text="Apply")
+        button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+        
+        self.root.mainloop()
     
     def show(self):
         pygame.init()
@@ -31,29 +62,43 @@ class RenderWindow:
         glUseProgram(self.shader_program)
         
         self.screenSize_loc = glGetUniformLocation(self.shader_program, "screenSize")
+        
+        tk_thread = threading.Thread(target=self._settingsWindow)
+        tk_thread.start()
+
+
         self._mainloop()
   
         
     def _mainloop(self):
         self.running = True
         self.start_time = time.time()
+        
+        self.modified = True
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-            
-            self.width, self.height = pygame.display.get_surface().get_size()
-            glUniform2f(self.screenSize_loc, self.width, self.height)
-            glClear(GL_COLOR_BUFFER_BIT)
+                    
+            if(self.modified):
+                self.width, self.height = pygame.display.get_surface().get_size()
+                glUniform2f(self.screenSize_loc, self.width, self.height)
+                glClear(GL_COLOR_BUFFER_BIT)
 
-            current_time = time.time() - self.start_time
-            time_uniform = glGetUniformLocation(self.shader_program, "time")
-            glUniform1f(time_uniform, current_time)
+                current_time = time.time() - self.start_time
+                time_uniform = glGetUniformLocation(self.shader_program, "time")
+                glUniform1f(time_uniform, current_time)
     
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+                glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
 
-            pygame.display.flip()
+                pygame.display.flip()
+                
+                self.modified = False
+            else:
+                pass
             
+
+
         self._terminate()
  
         
@@ -63,4 +108,5 @@ class RenderWindow:
         glDeleteBuffers(1, [self.vbo])
         
         pygame.quit()
+        self.root.quit()
         
