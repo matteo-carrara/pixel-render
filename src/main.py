3edb6295b4adedcddd0fbe25630183e00e744779
matteo carrara <matteo.carrara.7@gmail.com>
2023-09-19 14:16:01 +0200

    Moved shader code inside python file so it's easier to make an exe file

--- src/main.py
+++ src/main.py
@@ -5,12 +5,33 @@ from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 import time
 
+vertex_shader = """
+#version 330 core
+layout(location = 0) in vec2 in_position;
+void main() {
+    gl_Position = vec4(in_position, 0.0, 1.0);
+}
+"""
+fragment_shader = """
+#version 330 core
+out vec4 fragColor;
+uniform vec2 screenSize;
+
+void main() {
+    vec2 uv = gl_FragCoord.xy / screenSize;
+    if(uv.x > 0.3 && uv.x < 0.7 && uv.y > 0.5 && uv.y < 0.505)
+        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    else
+        fragColor = vec4(0.3, 0.3, 0.3, 1.0);
+}
+"""
+
 width, height = 800, 600 
+
 
 pygame.init()
 window = pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
-vertex_shader = ''.join([line for line in open("vtx-shader.glsl", 'r')])
-fragment_shader = ''.join([line for line in open("frg-shader.glsl", 'r')])
+
 shader_program = compileProgram(compileShader(vertex_shader, GL_VERTEX_SHADER), compileShader(fragment_shader, GL_FRAGMENT_SHADER))
 vao = glGenVertexArrays(1)
 vbo = glGenBuffers(1)
@@ -22,6 +43,7 @@ glEnableVertexAttribArray(0)
 glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
 glUseProgram(shader_program)
 screenSize_loc = glGetUniformLocation(shader_program, "screenSize")
+
 
 
 running = True
